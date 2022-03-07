dfbc90738d3e72060f2a3d0e520bec47edf456bd
David McNeary <djmcwebdev@gmail.com>
2022-03-07 14:55:28 -0800

    dm: db config

--- server/config/connect.js
+++ server/config/connect.js
@@ -7,7 +7,7 @@ const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
       host: 'localhost',
-      dialect: 'mysql',
+      dialect: process.env.DB_DIALECT,
       port: 3306
     });
 
