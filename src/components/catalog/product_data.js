507c174fef41ffefda7ecb7d89276ae69bd91191
anthony-schanen <schanenanthony@gmail.com>
2022-03-16 14:43:12 -0700

    anthony: applied prettier

--- src/components/catalog/product_data.js
+++ src/components/catalog/product_data.js
@@ -1,75 +1,75 @@
 const product_card = [
-    {
-        id: 1,
-        product_name: "Zingo Alpha",
-        description: "Your average heroku italian",
-        price: 350,
-        currency: "$",
-        thumb: "./product.png"
-    },
-    {
-        id: 2,
-        product_name: "Heroku Italian",
-        description: "Your average heroku italian",
-        price: 250,
-        currency: "$",
-        thumb: "./product.png"
-    },
-    {
-        id: 3,
-        product_name: "AWS Edge Machine",
-        description: "Your average heroku italian",
-        price: 350,
-        currency: "$",
-        thumb: "./product.png"
-    },
-    {
-        id: 4,
-        product_name: "Trash",
-        description: "Your average heroku italian",
-        price: 350,
-        currency: "$",
-        thumb: "./product.png"
-    },
-    {
-        id: 5,
-        product_name: "Sarah Blazic",
-        description: "Your average heroku italian",
-        price: 250,
-        currency: "$",
-        thumb: "./product.png"
-    },
-    {
-        id: 6,
-        product_name: "David Mcneary",
-        description: "Your average heroku italian",
-        price: 350,
-        currency: "$",
-        thumb: "./product.png"
-    },
-    {
-        id: 7,
-        product_name: "Jarod",
-        description: "Your average heroku italian",
-        price: 350,
-        currency: "$",
-        thumb: "./product.png"
-    },
-    {
-        id: 8,
-        product_name: "ANTON",
-        description: "Your average heroku italian",
-        price: 250,
-        currency: "$",
-        thumb: "./product.png"
-    },
-    {
-        id: 9,
-        product_name: "VERMA",
-        description: "Your average heroku italian",
-        price: 350,
-        currency: "$",
-        thumb: "./product.png"
-    }
-]
+  {
+    id: 1,
+    product_name: "Zingo Alpha",
+    description: "Your average heroku italian",
+    price: 350,
+    currency: "$",
+    thumb: "./product.png",
+  },
+  {
+    id: 2,
+    product_name: "Heroku Italian",
+    description: "Your average heroku italian",
+    price: 250,
+    currency: "$",
+    thumb: "./product.png",
+  },
+  {
+    id: 3,
+    product_name: "AWS Edge Machine",
+    description: "Your average heroku italian",
+    price: 350,
+    currency: "$",
+    thumb: "./product.png",
+  },
+  {
+    id: 4,
+    product_name: "Trash",
+    description: "Your average heroku italian",
+    price: 350,
+    currency: "$",
+    thumb: "./product.png",
+  },
+  {
+    id: 5,
+    product_name: "Sarah Blazic",
+    description: "Your average heroku italian",
+    price: 250,
+    currency: "$",
+    thumb: "./product.png",
+  },
+  {
+    id: 6,
+    product_name: "David Mcneary",
+    description: "Your average heroku italian",
+    price: 350,
+    currency: "$",
+    thumb: "./product.png",
+  },
+  {
+    id: 7,
+    product_name: "Jarod",
+    description: "Your average heroku italian",
+    price: 350,
+    currency: "$",
+    thumb: "./product.png",
+  },
+  {
+    id: 8,
+    product_name: "ANTON",
+    description: "Your average heroku italian",
+    price: 250,
+    currency: "$",
+    thumb: "./product.png",
+  },
+  {
+    id: 9,
+    product_name: "VERMA",
+    description: "Your average heroku italian",
+    price: 350,
+    currency: "$",
+    thumb: "./product.png",
+  },
+];
 export default product_card;
