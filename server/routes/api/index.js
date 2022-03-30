bf2052547f3b2edee4609f48a21bb1c999b1f4d1
David McNeary <djmcwebdev@gmail.com>
2022-03-30 15:04:47 -0700

    dm:product route and model

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -3,9 +3,11 @@ const router = require('express').Router();
 const userRoutes = require('./users');
 const orderRoutes = require('./orders');
 const reviewRoutes = require('./reviews');
+const productRoutes = require('./products');
 
 router.use('/users', userRoutes);
 router.use('/orders', orderRoutes);
 router.use('/reviews', reviewRoutes);
+router.use('/products', productsRoutes);
 
 module.exports = router;
