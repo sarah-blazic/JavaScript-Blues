b5502c673bc0f5a05e7dd9873dda6d73cee5e9ea
Syber <SyberiousBlast@gmail.com>
2022-04-06 11:02:22 -0700

    Finished account page.

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -4,45 +4,14 @@ import "./Footer.css";
 class Footer extends Component {
     constructor(props) {
         super(props);
-        const products = {
-            "Frogs":"#",
-            "Toads":"#",
-            "Green Amphibians":"#",
-            "Tadpoles?":"#"
-        };
-        const socials = {
-            "Contact Us":"#",
-            "Facebook":"#",
-            "Twitter":"#",
-            "Youtube":"#"
-        };
-        let key = "";
-        this.productLinks = [];
-        for (key in products) {
-            this.productLinks.push(
-                <li><a href={products[key]} className="footer-link">{key}</a></li>
-            );
-        }
-        this.socialLinks = [];
-        for (key in socials) {
-            this.socialLinks.push(
-                <li><a href={socials[key]} className="footer-link">{key}</a></li>
-            );
-        }
     }
 
     render () {
         return (<div className="footer-content">
-            <div className="padding"></div>
-            <div className="footer-ul-container">
-                <ul className="category">
-                    <li className="category-title">Products</li>
-                    {this.productLinks}
-                </ul>
-                <ul className="category">
-                    <li className="category-title">Socials</li>
-                    {this.socialLinks}
-                </ul>
+            <div className="icons-container">
+                <i class="fa-brands fa-twitter-square fa-2xl icon"></i>
+                <i class="fa-brands fa-facebook-square fa-2xl icon"></i>
+                <i class="fa-brands fa-instagram-square fa-2xl icon"></i>
             </div>
         </div>);
     }
