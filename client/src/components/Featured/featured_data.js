24ff5ed84ad521a7c7a7669b2fd317ddf645c51f
Jarod Wilson <syber@Jarods-MacBook-Air.local>
2022-04-27 14:02:12 -0700

    jarod: all classes are now working functional components

--- client/src/components/Featured/featured_data.js
+++ client/src/components/Featured/featured_data.js
@@ -1,22 +1,25 @@
-const featured_images = {
-    total: 4,
-    _1: {
+const featured_images = [
+    {
+      id: 1,
       name: "frog1",
       uri: require("../../images/frog1.jpg")
     },
-    _2: {
+    {
+      id: 2,
       name: "frog2",
       uri: require("../../images/frog2.jpg")
     },
-    _3: {
+    {
+      id: 3,
       name: "frog3",
       uri: require("../../images/frog3.jpg")
     },
-    _4: {
+    {
+      id: 4,
       name: "frog4",
       uri: require("../../images/frog4.jpg")
     }
-};
+];
 
 export default featured_images;
   
