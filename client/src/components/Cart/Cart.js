c25329757a6341f212b330c93a4f96cc5d2638f5
David McNeary <djmcwebdev@gmail.com>
2022-05-02 15:09:36 -0700

    minor bugfixes

--- client/src/components/Cart/Cart.js
+++ client/src/components/Cart/Cart.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Cart.css";
 import { Card } from "@mui/material";
-import product_card from "../catalog/product_data";
+// import product_card from "../catalog/product_data";
 import logo from "./product.png";
 // import "bootstrap/dist/css/bootstrap.min.css";
 import { Container, Row, Col } from "react-bootstrap";
--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,20 +1,16 @@
-import React, { Component } from 'react';
+import React from "react";
 import "./Footer.css";
 
-class Footer extends Component {
-    constructor(props) {
-        super(props);
-    }
-
-    render () {
-        return (<div className="footer-content">
-            <div className="icons-container">
-                <i class="fa-brands fa-twitter-square fa-2xl icon"></i>
-                <i class="fa-brands fa-facebook-square fa-2xl icon"></i>
-                <i class="fa-brands fa-instagram-square fa-2xl icon"></i>
-            </div>
-        </div>);
-    }
+function Footer() {
+  return (
+    <div className="footer-content">
+      <div className="icons-container">
+        <i class="fa-brands fa-twitter-square fa-2xl icon"></i>
+        <i class="fa-brands fa-facebook-square fa-2xl icon"></i>
+        <i class="fa-brands fa-instagram-square fa-2xl icon"></i>
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
