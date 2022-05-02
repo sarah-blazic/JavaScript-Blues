31b26142234228e419bb9da0fbdc32db3669b52b
David McNeary <djmcwebdev@gmail.com>
2022-05-02 14:07:08 -0700

    merge conflict

--- client/src/App.js
+++ client/src/App.js
@@ -1,57 +1,57 @@
 import "./App.css";
 import React, { useContext } from "react";
-import { BrowserRouter, Route, Link, Routes, Navigate } from "react-router-dom";
+import { useNavigate, Route, Routes, Navigate } from "react-router-dom";
 import { AuthProvider, AuthContext } from "./AuthContext";
 import MainPage from "./components/MainPage/MainPage";
 import Account from "./components/Account/Account";
-import Button from "./components/Button.js";
 import Navbar from "./components/Navbar/Navbar";
 import Login from "./components/Login/Login";
-import SignUp from "./components/SignUp/Sign-Up";
+import Signup from "./components/Signup/Signup";
 import Catalog from "./components/catalog/Catalog";
 import Footer from "./components/Footer/Footer";
 
 function App() {
-  const adminUser = {};
-  const { isAuth, setIsAuth } = useContext(AuthContext);
+  const navigate = useNavigate();
+  const { isAuth } = useContext(AuthContext);
   console.log("App auth: ", isAuth);
 
-  // here we are ceating a private route wrapper to prevent front end routing to 
+  // here we are ceating a private route wrapper to prevent front end routing to
   // restricted pages.  The ({ component: Component, ...rest })  argument that is
-  // passed to this functional component is essentially the same as just passing 
-  // props, but using object destucturing.  the ...rest is literally the rest of 
-  // the props that were not destructured. 
+  // passed to this functional component is essentially the same as just passing
+  // props, but using object destucturing.  the ...rest is literally the rest of
+  // the props that were not destructured.
   const RequireAuth = ({ children, redirectTo, ...rest }) => {
-      return isAuth ? children : <Navigate to={ redirectTo } />;
+    return isAuth ? children : <Navigate to={redirectTo} />;
   };
 
   return (
-    <BrowserRouter>
-      <div className="App">
-        <Navbar />
-        <Routes>
-          <Route exact path="/" element={ <MainPage /> } />
-          <Route path="/catalog" element={ <Catalog /> } />
-          <Route path="/login" element={ <Login /> } />
-          <Route path="/signup" element={ <SignUp />} />
-          <Route path="/account" 
-            element={
-              <RequireAuth redirectTo="/login">
-                <Account />
-              </RequireAuth>
-            }
-          />
-        </Routes>
-        <Footer />
-      </div>
-    </BrowserRouter>
+    <div className="App">
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<MainPage />} />
+        <Route path="/catalog" element={<Catalog />} />
+        <Route path="/login" element={<Login navigate={navigate} />} />
+        <Route path="/signup"  element={<Signup navigate={navigate} />} />
+        <Route
+          path="/account"
+          element={
+            <RequireAuth redirectTo="/login">
+              <Account />
+            </RequireAuth>
+          }
+        />
+      </Routes>
+      <Footer />
+    </div>
   );
 }
 
-export default () => {
+function Main() {
   return (
     <AuthProvider>
       <App />
     </AuthProvider>
   );
-};
+}
+
+export default Main;
--- server/routes/api/users.js
+++ server/routes/api/users.js
@@ -1,19 +1,13 @@
-const {
-  login,
-  signup,
-  logout,
-  getData
-} = require('../../controllers/user');
+const { login, signup, logout, getData } = require("../../controllers/user");
 const router = require("express").Router();
 const passport = require("../../config/passport");
 
+router.post("/login", passport.authenticate("local"), login);
 
-  router.post("/login", passport.authenticate("local"), login);
+router.post("/signup", signup);
 
-  router.post("/signup", signup);
+router.get("/logout", logout);
 
-  router.get("/logout", logout);
-
-  router.get("/user_data", getData);
+router.get("/user_data", getData);
 
 module.exports = router;
