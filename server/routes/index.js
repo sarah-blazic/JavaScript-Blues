f854177b8a2f071cd848de068253f84434fc6107
David McNeary <djmcwebdev@gmail.com>
2022-05-10 19:26:59 -0700

    modularize stripe controller

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,80 +1,13 @@
 const path = require("path");
 const router = require("express").Router();
 const apiRoutes = require("./api/index");
-const stripe = require('stripe')('sk_test_51KsvsUGHngg3wb7931XRlk9xsus6vbnxTavV0EGdtBVTX1A6EjXsrfJS4gHxJ90dWf4WVmfydIAOtmSep49SFM1q00InFGLsyD')
+const stripe = require("./stripe");
 
 // API Routes
 router.use('/api', apiRoutes);
 
 // stripe
-router.post('/create-checkout-session', async (req, res) => {
-  const session = await stripe.checkout.sessions.create({
-    shipping_address_collection: {
-      allowed_countries: ['US', 'CA'],
-    },
-    shipping_options: [
-      {
-        shipping_rate_data: {
-          type: 'fixed_amount',
-          fixed_amount: {
-            amount: 0,
-            currency: 'usd',
-          },
-          display_name: 'Free shipping',
-          // Delivers between 5-7 business days
-          delivery_estimate: {
-            minimum: {
-              unit: 'business_day',
-              value: 5,
-            },
-            maximum: {
-              unit: 'business_day',
-              value: 7,
-            },
-          }
-        }
-      },
-      {
-        shipping_rate_data: {
-          type: 'fixed_amount',
-          fixed_amount: {
-            amount: 1500,
-            currency: 'usd',
-          },
-          display_name: 'Next day air',
-          // Delivers in exactly 1 business day
-          delivery_estimate: {
-            minimum: {
-              unit: 'business_day',
-              value: 1,
-            },
-            maximum: {
-              unit: 'business_day',
-              value: 1,
-            },
-          }
-        }
-      },
-    ],
-    line_items: [
-      {
-        price_data: {
-          currency: 'usd',
-          product_data: {
-            name: 'T-shirt',
-          },
-          unit_amount: 2000,
-        },
-        quantity: 1,
-      },
-    ],
-    mode: 'payment',
-    success_url: 'https://example.com/success',
-    cancel_url: 'https://example.com/cancel',
-  });
-
-  res.redirect(303, session.url);
-});
+router.post('/create-checkout-session', stripe.stripeSession);
 
 // If no API routes are hit, send the React app
 router.use((req, res) => {
---
+++ server/routes/stripe.js
@@ -0,0 +1,73 @@
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
+module.exports = {
+    
+    stripeSession: async (req, res) => {
+    const session = await stripe.checkout.sessions.create({
+      shipping_address_collection: {
+        allowed_countries: ['US', 'CA'],
+      },
+      shipping_options: [
+        {
+          shipping_rate_data: {
+            type: 'fixed_amount',
+            fixed_amount: {
+              amount: 0,
+              currency: 'usd',
+            },
+            display_name: 'Free shipping',
+            // Delivers between 5-7 business days
+            delivery_estimate: {
+              minimum: {
+                unit: 'business_day',
+                value: 5,
+              },
+              maximum: {
+                unit: 'business_day',
+                value: 7,
+              },
+            }
+          }
+        },
+        {
+          shipping_rate_data: {
+            type: 'fixed_amount',
+            fixed_amount: {
+              amount: 1500,
+              currency: 'usd',
+            },
+            display_name: 'Next day air',
+            // Delivers in exactly 1 business day
+            delivery_estimate: {
+              minimum: {
+                unit: 'business_day',
+                value: 1,
+              },
+              maximum: {
+                unit: 'business_day',
+                value: 1,
+              },
+            }
+          }
+        },
+      ],
+      line_items: [
+        {
+          price_data: {
+            currency: 'usd',
+            product_data: {
+              name: 'T-shirt',
+            },
+            unit_amount: 2000,
+          },
+          quantity: 1,
+        },
+      ],
+      mode: 'payment',
+      success_url: 'https://example.com/success',
+      cancel_url: 'https://example.com/cancel',
+    });
+  
+    res.redirect(303, session.url);
+  }
+};
