03c2e17191348174e1dc8aa126fde1d55485262a
David McNeary <43227293+dmcneary@users.noreply.github.com>
2022-05-10 23:26:58 -0700

    Revert "Added functional product page and functional view product button on catalog."

--- client/src/components/ProductCard/ProductCard.js
+++ client/src/components/ProductCard/ProductCard.js
@@ -1,25 +1,14 @@
-import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
-import Grid from "@mui/material/Grid";
-import Divider from "@mui/material/Divider";
-import Typography from "@mui/material/Typography";
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Grid from '@mui/material/Grid';
+import Divider from '@mui/material/Divider';
+import Typography from '@mui/material/Typography';
 import "./ProductCard.css";
-import axios from "axios";
-import { useParams, Link } from "react-router-dom";
-import ProductPage from "../../pages/ProductPage/ProductPage";
 
 const ProductCard = ({ onAdd, item }) => {
   return (
-    <Box
-      sx={{
-        width: "100%",
-        maxWidth: 360,
-        bgcolor: "background.paper",
-        boxShadow: 2,
-        mb: 3,
-      }}
-    >
-      <Box sx={{ my: 3, mx: 2 }}>
+    <Box sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper', boxShadow: 2, mb: 3}}>
+      <Box sx={{ my: 3, mx: 2, pt: 1 }}>
         <Grid container alignItems="center">
           <Grid item xs>
             <Typography gutterBottom variant="h5" component="div">
@@ -33,25 +22,15 @@ const ProductCard = ({ onAdd, item }) => {
           </Grid>
         </Grid>
       </Box>
-      <img
-        className="product-image"
-        src={"../../images/" + item.image}
-        alt={item.name}
-      />
-      <Divider variant="middle" sx={{ width: "90%", ml: "5%" }} />
-      <Typography color="text.secondary" variant="body2" sx={{ my: 3, mx: 2 }}>
+      <img className="product-image" src={"../../images/" + item.image} alt={item.name}/>
+      <Divider variant="middle" sx={{ width: '90%', ml: '5%' }}/>
+      <Typography color="text.secondary" variant="body2" sx={{my: 3, mx: 2}}>
         {item.description}
       </Typography>
       <Box sx={{ mt: 3, ml: 1, mb: 1 }}>
-        <Button onClick={() => onAdd(item)}>Add to cart</Button>
-        <Button
-          component={Link}
-          to={`/product/${item.id}`}
-        >
-          View Product
-        </Button>
+        <Button onClick={() => {onAdd(item)}}>Add to cart</Button>
       </Box>
     </Box>
   );
-};
+}
 export default ProductCard;
